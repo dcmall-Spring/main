a0674ecb68aea413181c2683dc3566b2f2c1c5b6
HaHuRyu <dlwjdgml4567@gmail.com>
2024-06-21 23:31:30 +0900

    크로링 수정

--- src/main/java/com/dcmall/back/Service/WebCrawlerService.java
+++ src/main/java/com/dcmall/back/Service/WebCrawlerService.java
@@ -16,52 +16,51 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class WebCrawlerService {
+
     @Autowired
     ProductInfoDAO dao;
 
     public void scrapeWebPageWithSelenium(String url) {
-        System.out.println("url");
         ArrayList<String> listTitle = new ArrayList<>();
         ArrayList<String> listUrl = new ArrayList<>();
         String product = this.dao.selectProduct(1);
 
-        /*
-            이럴거면 URL을 게시글 post 숫자만 파악해서 저장하는게 괜찮을거 같은데.. 고민좀 해봐야 될듯.
-         */
         int postNumber = 0;
-        if(product != null){
-            postNumber = Integer.parseInt(product.substring(45));
-        } 
-        
-        System.out.println(postNumber);
 
+        if(product != null){
+            postNumber = Integer.parseInt(product);
+        }
+        
         try {
             Document doc = Jsoup.connect(url).get();
 
             // subject-link 클래스를 가진 요소 선택
             Elements titles = doc.select(".ellipsis-with-reply-cnt");
             Elements urls = doc.select(".subject-link");
+            Elements costs = doc.select(".text-orange");
 
-
-            for (Element element : titles) {
-                // 요소 자체를 포함한 HTML 출력
-                listTitle.add(element.text());
-            }
-
-            for(Element element : urls){
-                if(element.childrenSize() > 0){
-                    listUrl.add("https://quasarzone.com" + element.attr("href"));
+            Collections.reverse(urls);
+            Collections.reverse(titles);
+            Collections.reverse(costs);
+            // braek
+            for(int i = 0; i < titles.size(); i ++){
+                if(Integer.parseInt(urls.get(i).attr("href").substring(23)) > postNumber){
+                    listTitle.add(titles.get(i).text());
+                    if(urls.get(i).childrenSize() > 0){
+                        listUrl.add(urls.get(i).attr("href").substring(23));
+                    }
                 }
             }
+
             // 출력할 요소를 list에 저장 후 db에 저장.
             for (int i = 0; i < listTitle.size(); i++) {
-                if(postNumber < Integer.parseInt(listUrl.get(i).substring(45)) && product != null){
-                   dao.insertProduct("1", listTitle.get(i), listUrl.get(i));
-                }
+                dao.insertProduct("1", listTitle.get(i), listUrl.get(i));
             }
 
         } catch (IOException e) {
