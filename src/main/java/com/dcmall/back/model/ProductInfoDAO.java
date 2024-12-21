0c5b47ee5c9da08655bf121e5ee1fc256568ce14
HaHuRyu <dlwjdgml4567@gmail.com>
2024-12-21 22:01:38 +0900

    check

--- src/main/java/com/dcmall/back/model/ProductInfoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.dcmall.back.model;
-
-import java.util.List;
-
-public interface ProductInfoDAO {
-    int insertProduct(String id, String title, String cost, String url);
-    int insertProductWithImage(String id, String title, String cost, String url, String imageUrl);
-    String selectProduct(int id);
-}
