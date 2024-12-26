71e44987937595917f71dff48cbb625e973c8faf
HaHuRyu <dlwjdgml4567@gmail.com>
2024-12-27 02:04:26 +0900

    discord

--- src/main/java/com/dcmall/back/Controller/WebController.java
+++ src/main/java/com/dcmall/back/Controller/WebController.java
@@ -1,7 +1,6 @@
 package com.dcmall.back.Controller;
 
 import com.dcmall.back.Service.NotificationService;
-import com.dcmall.back.Service.WebCrawlerService;
 import com.dcmall.back.model.EmbedDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +15,6 @@ import java.util.concurrent.CompletableFuture;
 @RestController
 public class WebController {
     @Autowired
-    public WebCrawlerService webCrawlerService;
-    @Autowired
     private EmbedDAO embedDAO;
     @Autowired
     private NotificationService notificationService;
@@ -29,56 +26,21 @@ public class WebController {
         if (result == null) {
             num = 0;
             System.out.println("null");
-        }
-        else if (result instanceof Integer) {
+        } else if (result instanceof Integer) {
             num = (Integer) result;
         } else {
             num = 0;
         }
+
         try {
-            CompletableFuture.runAsync(() -> {
-                try {
-                    webCrawlerService.scrapeQuasarzone("https://quasarzone.com/bbs/qb_saleinfo");
-                    System.out.println("Quasarzone crawling completed");
-                } catch (Exception e) {
-                    System.out.println("Quasarzone crawling error: " + e.getMessage());
-                }
-            /*}).thenRun(() -> {
-                try {
-                    webCrawlerService.scrapefmkorea("https://www.fmkorea.com/hotdeal");
-                    System.out.println("FMKorea crawling completed");
-                } catch (Exception e) {
-                    System.out.println("FMKorea crawling error: " + e.getMessage());
-                }*/
-            /*}).thenRun(() -> {
-                try {
-                    webCrawlerService.scrapeRuliWeb("https://m.ruliweb.com/market/board/1020");
-                    System.out.println("RuliWeb crawling completed");
-                } catch (Exception e) {
-                    System.out.println("RuliWeb crawling error: " + e.getMessage());
-                }
-            }).thenRun(() -> {
-                try {
-                    webCrawlerService.scrapeArcalive("https://arca.live/b/hotdeal/");
-                    System.out.println("Arcalive crawling completed");
-                } catch (Exception e) {
-                    System.out.println("Arcalive crawling error: " + e.getMessage());
-                }
-            }).thenRun(() ->{
-                try{
-                    notificationService.titleCompare(num);
-                    System.out.println("num" + num);
-                    System.out.println("Send Message completed");
-                }catch (Exception e) {
-                    System.out.println("Send Message error: " + e.getMessage());
-                }*/
-            }).join();
-
-            return "All scheduled crawling completed successfully";
+            notificationService.titleCompare(num);
+            System.out.println("Send Message completed");
         } catch (Exception e) {
-            return "Scheduled crawling error: " + e.getMessage();
+            System.out.println("Send Message error: " + e.getMessage());
         }
-    }
 
+        return "success";
+
+    }
 
 }
